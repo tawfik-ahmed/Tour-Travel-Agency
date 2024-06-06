8003740e3161505a5e214185748216baa3026160
Tawfik Ahmed <140659996+tawfik-ahmed@users.noreply.github.com>
2024-06-07 00:35:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 
-document.getElementById('menu-btn').addEventListener('click', _ => document.querySelector('.navbar').classList.toggle('active'));
+const toggleBtn = document.getElementById('menu-btn');
+const navbar = document.querySelector('.navbar');
+
+toggleBtn.addEventListener('click', _ => navbar.classList.toggle('active'));
+
+document.addEventListener('click', e => {
+    if (e.target !== toggleBtn && e.target !== navbar) navbar.classList.remove('active');
+});
 
 document.querySelector('.about .controls').addEventListener('click', (e) => {
     const src = e.target.dataset.src;
     if (src) document.querySelector('.about .video').src = src;
-})
+});
