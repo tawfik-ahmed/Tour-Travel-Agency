1275dc5f90337bac0be130b0b9a29148c9eb176a
Tawfik Ahmed <140659996+tawfik-ahmed@users.noreply.github.com>
2024-06-06 20:47:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-let navbar = document.querySelector('.header .navbar');
 
-document.getElementById('menu-btn').addEventListener('click', _ => navbar.classList.toggle('active'));
+document.getElementById('menu-btn').addEventListener('click', _ => document.querySelector('.navbar').classList.toggle('active'));
 
 document.querySelector('.about .controls').addEventListener('click', (e) => {
-
     const src = e.target.dataset.src;
     if (src) document.querySelector('.about .video').src = src;
 })
